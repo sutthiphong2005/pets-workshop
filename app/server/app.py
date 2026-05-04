41e99104835a72ab96ccc270701f4891766b7fab
sutthiphong2005 <sutthiphong2005@gmail.com>
2026-05-04 11:29:49 +0000

    add list of breeds api

--- app/server/app.py
+++ app/server/app.py
@@ -77,7 +77,18 @@ def get_dog(id: int) -> tuple[Response, int] | Response:
     
     return jsonify(dog)
 
-## HERE
+# Route to get all breeds   
+@app.route('/api/breeds', methods=['GET'])
+def get_breeds() -> Response:
+    breeds_query = db.session.query(Breed.id, Breed.name).all()
+    breeds_list: List[Dict[str, Any]] = [
+        {
+            'id': breed.id,
+            'name': breed.name
+        }
+        for breed in breeds_query
+    ]
+    return jsonify({'breeds': breeds_list})
 
 if __name__ == '__main__':
     app.run(debug=True, port=5100) # Port 5100 to avoid macOS conflicts
